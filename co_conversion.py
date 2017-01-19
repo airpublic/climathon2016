8da06891a59b4adee8bcb62894fc519cbe1b1341
Lev Konstantinovskiy <lev.konst@gmail.com>
2017-01-19 11:41:32 -0300

    Update co_conversion.py

--- co_conversion.py
+++ co_conversion.py
@@ -3,7 +3,7 @@ ANALOG_REF_VOLTAGE = 3.3;
     # sensor #1
 CO_WORKING_ELECTRODE_ZERO_OFFSET_MV  = 310;
 CO_AUXILIARY_ELECTRODE_ZERO_OFFSET_MV  = 272;
-CO_SENSITIVITY = 0.197;
+CO_SENSITIVITY = 0.197; # mV/ppb
 # reverse to raw readings
 def reverse_co(CO_working=None, CO_aux=None, ppbCO=None):
     
@@ -21,7 +21,7 @@ reverse_co(ppbCO = 755.324, CO_aux = 349.612, CO_working = 458.793),
 reverse_co(ppbCO = 882.585, CO_aux = 349.344, CO_working = 483.866)]
 merged_with_kings = zip(readings,(200,300,400))
 
-### Trying to reproduce kings...
+### This gives right trend and right order of magnitude
 
 for x in merged_with_kings:
-    print ((x[0][0] - 33) +  (x[0][2] + 5)  )*CO_SENSITIVITY
+    print (x[0][0] -  (x[0][2] )  )/CO_SENSITIVITY, x[1] 
